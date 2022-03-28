59173ab4691e305c2b3423b8861cc7395aa3e763
magdalenarajic <57672483+magdalenarajic@users.noreply.github.com>
2022-03-28 13:25:40 +0200

    Add files via upload

--- cypress/integration/trakt/authorization.spec.js
+++ cypress/integration/trakt/authorization.spec.js
@@ -5,7 +5,7 @@ describe('01: Trakt API Authorization', () => {
 		cy.wrap(Cypress.env('client_id')).as('client_id');
 		cy.wrap(Cypress.env('client_secret')).as('client_secret');
 	});
-
+    
 	describe('01_001: Device Authentication Code and Access Token Authorization', () => {
 		it('01_001_001: Should obtain access tokens', function () {
 			cy.request({
@@ -20,6 +20,7 @@ describe('01: Trakt API Authorization', () => {
 				},
 			}).then($response => {
 				expect($response.status).to.be.eq(200);
+				expect($response.body).to.have.property('device_code');
 				expect($response.body).to.have.property('user_code');
 				expect($response.body).to.have.property('verification_url');
 				expect($response.body).to.have.property('expires_in');
@@ -40,9 +41,50 @@ describe('01: Trakt API Authorization', () => {
 							client_id: this.client_id,
 							client_secret: this.client_secret,
 						},
-					})
-				}) 
-			})
-		})
-    })
-})
+					}).then($tokenResponse => {
+						console.log($tokenResponse.body);
+						expect($tokenResponse.status).to.be.eq(200);
+						expect($tokenResponse.body).to.have.property('access_token');
+						expect($tokenResponse.body).to.have.property('refresh_token');
+					});
+				});
+			});
+		});
+		
+		it('01_001_002: Should not allow authentication without verifying device code', function () {
+			cy.request({
+				method: 'POST',
+				url: 'https://api.trakt.tv/oauth/device/code',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: {
+					client_id: Cypress.env('client_id'),
+				},
+			}).then($response => {
+				expect($response.status).to.be.eq(200);
+				expect($response.body).to.have.property('user_code');
+				expect($response.body).to.have.property('device_code');
+				expect($response.body).to.have.property('verification_url');
+				expect($response.body).to.have.property('expires_in');
+				expect($response.body).to.have.property('interval');
+
+				cy.request({
+					method: 'POST',
+					url: 'https://api.trakt.tv/oauth/device/token',
+					headers: {
+						'Content-Type': 'application/json',
+					},
+					body: {
+						code: $response.body.device_code,
+						client_id: this.client_id,
+						client_secret: this.client_secret,
+					},
+					failOnStatusCode: false,
+				}).then($tokenResponse => {
+					expect($tokenResponse.status).to.be.eq(400);
+				});
+			});
+		});	
+    });
+});
