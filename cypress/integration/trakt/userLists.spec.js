8f8d9eca5f18f1547efcc904af2bc0d76a6ad76f
magdalenarajic <57672483+magdalenarajic@users.noreply.github.com>
2022-03-31 19:17:04 +0200

    Commit

--- cypress/integration/trakt/userLists.spec.js
+++ cypress/integration/trakt/userLists.spec.js
@@ -1,49 +1,18 @@
 /// <reference types="cypress" />
-describe('02: Trakt API User list ', () => { 
-    describe('02_001: Create, Update and Delete operations with users lists', () => { 
-        before(function () {
-            cy.getAccessToken();
-        });
+describe('02: Trakt API User list ', () => {
+	describe('02_001: Create, Update and Delete operations with users lists', () => {
+		before(function () {
+			cy.getAccessToken()
+		})
 
-		it('02_001_001: Should delete all User Lists', () => {
-		    cy.deleteAllLists();
-		});
+		it.only('02_001_001: Should delete all User Lists', () => {
+			cy.deleteAllLists().then(resp => {
+				expect(resp.status).to.be.eq(200)
+			})
+		})
 
-        it('02_001_002: Should create private user list and not allow to see for user without authorization', () => {
-            cy.createUserList('My first private list', 'private');
-            cy.request({
-				method: 'GET',
-				url: `https://api.trakt.tv/users/${Cypress.env('user').username}/lists`,
-				headers: {
-					'Content-Type': 'application/json',
-					'trakt-api-version': Cypress.env('trakt_api_version'),
-					'trakt-api-key': Cypress.env('client_id'),
-				},
-				failOnStatusCode: false,
-			}).then($userCustomList => {
-				expect($userCustomList.body).to.be.empty;
-			});
-        })
-        
-        it('02_001_003: Should show private list for authorized user', () => {
-            cy.request({
-				method: 'GET',
-				url: `https://api.trakt.tv/users/${Cypress.env('user').username}/lists`,
-				headers: {
-					'Content-Type': 'application/json',
-					Authorization: `Bearer ${Cypress.env('access_token')}`,
-					'trakt-api-version': Cypress.env('trakt_api_version'),
-					'trakt-api-key': Cypress.env('client_id'),
-				},
-				failOnStatusCode: false,
-			}).then($userCustomList => {
-				expect($userCustomList.status).to.be.eq(200);
-				expect($userCustomList.body[0].name).to.be.eq('My first private list');
-			});
-        })
-
-        it('02_001_004: Should create public user list and get that list without authorization', () => {
-            cy.createUserList('My public list-Star Wars', 'public')
+		it('02_001_002: Should create private user list and not allow to see for user without authorization', () => {
+			cy.createUserList('My first private list', 'private')
 			cy.request({
 				method: 'GET',
 				url: `https://api.trakt.tv/users/${Cypress.env('user').username}/lists`,
@@ -53,11 +22,46 @@ describe('02: Trakt API User list ', () => {
 					'trakt-api-key': Cypress.env('client_id'),
 				},
 				failOnStatusCode: false,
-			}).then($userCustomList => {
-				expect($userCustomList.status).to.be.eq(200);
-				expect($userCustomList.body[0].name).to.be.eq('My public list-Star Wars');
-			});
-        });
+			}).then(userCustomList => {
+				expect(userCustomList.body).to.be.empty
+			})
+		})
+
+		it('02_001_003: Should show private list for authorized user', () => {
+			cy.request({
+				method: 'GET',
+				url: `https://api.trakt.tv/users/${Cypress.env('user').username}/lists`,
+				headers: {
+					'Content-Type': 'application/json',
+					Authorization: `Bearer ${Cypress.env('access_token')}`,
+					'trakt-api-version': Cypress.env('trakt_api_version'),
+					'trakt-api-key': Cypress.env('client_id'),
+				},
+				failOnStatusCode: false,
+			}).then(userCustomList => {
+				expect(userCustomList.status).to.be.eq(200)
+				expect(userCustomList.body[0].name).to.be.eq('My first private list')
+			})
+		})
+
+		it('02_001_004: Should create public user list and get that list without authorization', () => {
+			cy.createUserList('My public list-Star Wars', 'public')
+			cy.request({
+				method: 'GET',
+				url: `https://api.trakt.tv/users/${Cypress.env('user').username}/lists`,
+				headers: {
+					'Content-Type': 'application/json',
+					'trakt-api-version': Cypress.env('trakt_api_version'),
+					'trakt-api-key': Cypress.env('client_id'),
+				},
+				failOnStatusCode: false,
+			}).then(userCustomList => {
+				expect(userCustomList.status).to.be.eq(200)
+				expect(userCustomList.body[0].name).to.be.eq(
+					'My public list-Star Wars'
+				)
+			})
+		})
 
 		it('02_001_005: Should get all user lists', () => {
 			cy.request({
@@ -70,12 +74,12 @@ describe('02: Trakt API User list ', () => {
 					'trakt-api-key': Cypress.env('client_id'),
 				},
 				failOnStatusCode: false,
-			}).then($response => {
-                expect($response.status).to.be.eq(200);
-			});
-		});
+			}).then(response => {
+				expect(response.status).to.be.eq(200)
+			})
+		})
 
-        it('02_001_006: Should successfully delete user list', () => {
+		it('02_001_006: Should successfully delete user list', () => {
 			cy.request({
 				method: 'GET',
 				url: `https://api.trakt.tv/users/mrajic/lists`,
@@ -86,14 +90,17 @@ describe('02: Trakt API User list ', () => {
 					'trakt-api-key': Cypress.env('client_id'),
 				},
 				failOnStatusCode: false,
-			}).then($userCustomList => {
-				expect($userCustomList.status).to.be.eq(200);
-				expect($userCustomList.body[1].name).to.be.eq('My public list-Star Wars');
+			}).then(userCustomList => {
+				expect(userCustomList.status).to.be.eq(200)
+				expect(userCustomList.body[1].name).to.be.eq(
+					'My public list-Star Wars'
+				)
 
-			    cy.request({
+				cy.request({
 					method: 'DELETE',
 					url: `https://api.trakt.tv/users/${
-						Cypress.env('user').username}/lists/${$userCustomList.body[1].ids.slug}`,
+						Cypress.env('user').username
+					}/lists/${userCustomList.body[1].ids.slug}`,
 					headers: {
 						'Content-Type': 'application/json',
 						Authorization: `Bearer ${Cypress.env('access_token')}`,
@@ -101,11 +108,11 @@ describe('02: Trakt API User list ', () => {
 						'trakt-api-key': Cypress.env('client_id'),
 					},
 					failOnStatusCode: false,
-				}).then($response => {
-					expect($response.status).to.be.eq(204); 
-			    });
-            });
-		});
+				}).then(response => {
+					expect(response.status).to.be.eq(204)
+				})
+			})
+		})
 
 		it('02_001_007: Should successfully update one user list', () => {
 			cy.request({
@@ -118,14 +125,15 @@ describe('02: Trakt API User list ', () => {
 					'trakt-api-key': Cypress.env('client_id'),
 				},
 				failOnStatusCode: false,
-			}).then($userCustomList => {
-				expect($userCustomList.status).to.be.eq(200);
-				expect($userCustomList.body[0].name).to.be.eq('My first private list');
+			}).then(userCustomList => {
+				expect(userCustomList.status).to.be.eq(200)
+				expect(userCustomList.body[0].name).to.be.eq('My first private list')
 
-			    cy.request({
+				cy.request({
 					method: 'PUT',
 					url: `https://api.trakt.tv/users/${
-						Cypress.env('user').username}/lists/${$userCustomList.body[0].ids.slug}`,
+						Cypress.env('user').username
+					}/lists/${userCustomList.body[0].ids.slug}`,
 					headers: {
 						'Content-Type': 'application/json',
 						Authorization: `Bearer ${Cypress.env('access_token')}`,
@@ -133,18 +141,18 @@ describe('02: Trakt API User list ', () => {
 						'trakt-api-key': Cypress.env('client_id'),
 					},
 					failOnStatusCode: false,
-					body: { name: 'Updated list'},
-				}).then($response => {
-					expect($response.status).to.be.eq(200); 
-			    });
-            });
-		});
-    });
+					body: { name: 'Updated list' },
+				}).then(response => {
+					expect(response.status).to.be.eq(200)
+				})
+			})
+		})
+	})
 
-	describe('02_002: Invalid scenarios with user lists', () => { 	
+	describe('02_002: Invalid scenarios with user lists', () => {
 		before(function () {
-            cy.getAccessToken();
-        });
+			cy.getAccessToken()
+		})
 
 		it('02_002_001: Should return 401 when not authorized for create list', () => {
 			cy.request({
@@ -166,15 +174,17 @@ describe('02: Trakt API User list ', () => {
 					sort_how: 'asc',
 				},
 				failOnStatusCode: false,
-			}).then($response => {
-				expect($response.status).to.be.eq(401);		
-			});
-		});
+			}).then(response => {
+				expect(response.status).to.be.eq(401)
+			})
+		})
 
 		it('02_002_002: Should return 404 when trying to delete list which not exist', () => {
 			cy.request({
 				method: 'DELETE',
-				url: `https://api.trakt.tv/users/${Cypress.env('user').username}/lists/some-list`,
+				url: `https://api.trakt.tv/users/${
+					Cypress.env('user').username
+				}/lists/some-list`,
 				headers: {
 					'Content-Type': 'application/json',
 					Authorization: `Bearer ${Cypress.env('access_token')}`,
@@ -182,10 +192,10 @@ describe('02: Trakt API User list ', () => {
 					'trakt-api-key': Cypress.env('client_id'),
 				},
 				failOnStatusCode: false,
-			}).then($response => {
-				expect($response.status).to.be.eq(404);		
-			});
-		});
+			}).then(response => {
+				expect(response.status).to.be.eq(404)
+			})
+		})
 
 		it('02_002_003: Should return 422 when trying to create list with no body', () => {
 			cy.request({
@@ -198,9 +208,9 @@ describe('02: Trakt API User list ', () => {
 					'trakt-api-key': Cypress.env('client_id'),
 				},
 				failOnStatusCode: false,
-			}).then($response => {
-				expect($response.status).to.be.eq(422);		
-			});
-		});
-	});
-});
+			}).then(response => {
+				expect(response.status).to.be.eq(422)
+			})
+		})
+	})
+})
